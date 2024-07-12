468539a15d4050b8dbac25753fa69ba1ee557121
codigojv <codigojv11@gmail.com>
2024-07-12 10:58:09 -0600

    Full commit of complete project

---
+++ src/test/java/com/ar11/forohub/ForohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ar11.forohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
